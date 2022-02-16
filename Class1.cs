04ee0dc6e858472557c03879f0b0e12583cdfda5
Timosha145 <93324319+Timosha145@users.noreply.github.com>
2022-02-16 23:24:26 +0200

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,20 @@ namespace classTasks_topolja
 {
     class Class1
     {
+        public static List<string> suggestFood(double BOO, List<string> food, List<int> calories)
+        {
+            Random rnd = new Random();
+            int nCalories = 0;
+            List<string> foodList = new List<string>();
+            do
+            {
+                int randN = rnd.Next(1, calories.Count);
+                int a=calories[randN];
+                nCalories += a;
+                foodList.Add(food[randN]);
+            } while (nCalories+100<BOO);
+
+            return foodList;
+        }
     }
 }
